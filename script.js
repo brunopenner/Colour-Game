ccbc878a3690cee76d7510a90b3827b7381b9b29
Bruno Penner <brunopenner01@gmail.com>
2024-08-19 12:11:12 +0800

    Adding Winner logic

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
-function generateNumber() {
-    let min = 0;
-    let max = 255;
+//
+function generateNumber(x=255) {
+    let max = x;
     let randomNumber = Math.floor(Math.random() * (max + 1));
 
     return randomNumber;
@@ -12,11 +12,34 @@ function addBgColour(square) {
     square.style.backgroundColor=rgb;
 }
 
-const correctColour = 'RGB (' + generateNumber() + ', ' + generateNumber() + ', ' + generateNumber() + ')';
-console.log('Correct Colour: ' + correctColour);
-document.getElementById('correct-colour').textContent = correctColour;
+function addWinnerColour() {
+    document.getElementById("hero").style.backgroundColor = correctColour
+    document.querySelectorAll(".colour-square").forEach(squareWinner => {
+        console.log("Correct Colour: " + correctColour)
+        squareWinner.style.backgroundColor = correctColour;
+    })
+}
 
-const squareList = document.querySelectorAll(".colour-square");
-console.log(squareList);
+let gamePlay = true;
+const correctColour = 'rgb(' + generateNumber() + ', ' + generateNumber() + ', ' + generateNumber() + ')';
+if(gamePlay) {
+    console.log('Correct Colour: ' + correctColour);
+    document.getElementById('correct-colour').textContent = correctColour;
 
-squareList.forEach(addBgColour);
+    const squareList = document.querySelectorAll(".colour-square");
+    console.log(squareList);
+
+    squareList.forEach(addBgColour);
+    let modifiedSquare = squareList[generateNumber(5)].style.backgroundColor= correctColour;
+
+    squareList.forEach(square => {
+        square.addEventListener('click', () =>  {
+            console.log(square.style.backgroundColor);
+            if(square.style.backgroundColor == correctColour) {
+                addWinnerColour();
+                console.log("Winner!");
+            }
+        })
+    })
+}
+
